2b03c1de21d17c289b4b2b00d207eff9e0df920a
Kevin Vosburgh <kvosbur@purdue.edu>
2018-02-08 16:03:00 -0500

    Created prototypes for database methods to allow calls within other classes. Also created database object within Server class. There are slight updates here for the database sql creation commands.

--- src/main/java/LambencyServer.java
+++ src/main/java/LambencyServer.java
@@ -2,12 +2,30 @@ import static spark.Spark.*;
 
 public class LambencyServer{
 
+    DatabaseConnection dbc = null;
+
+    LambencyServer(){
+
+        try {
+            this.dbc = new DatabaseConnection();
+        }catch(Exception e){
+            //error happened in connecting to database
+        }
+
+        addroutes();
+    }
+
+    public void addroutes(){
+        // example of responding with a json object made from a java object
+        get("/User/login/google", "application/json", (request, response) -> {
+            Test t = new Test();
+            return t.array;
+        }, new JsonTransformer());
+    }
+
     public static void main(String[]args){
 
+        LambencyServer lb = new LambencyServer();
 
-        // example of responding with a json object made from a java object
-        get("/hello", "application/json", (request, response) -> {
-            return new Test();
-        }, new JsonTransformer());
     }
 }
