70048f2ef730da80fcc5cbc267a0328d7d0b8f6f
zmswartz <zmswartz@users.noreply.github.com>
2018-02-11 14:28:51 -0500

    Zack facebook login (#6)
    * Started this test branch
    
    * Added Google LoginHandler
    Added UserAuthenticator
    
    * Added Google LoginHandler
    
    * update server example
    
    * added url for testing and for documentation
    
    * Test commit
    
    * pulled dev database prototypes into this branch
    
    * FacebookLogin partial written
    
    * Created database method for creating users for either a facebook profile or google profile
    
    * Created Database method for searching for user (either by facebookid/googleid/userid or
    by oauthCode). Created Database method for setting oauthCode. Created User data class
    with getters and setters and a toString method for printing class.
    
    * FacebookLogin 1.2.
    
    * FacebookLogin 1.3

---
+++ src/main/java/FacebookLogin.java
@@ -0,0 +1,45 @@
+import java.sql.SQLException;
+
+public class FacebookLogin {
+    /**
+     *
+     * @param facebookId id from facebook API for a specific user
+     * @param firstName users first name
+     * @param lastName users last name
+     * @param email usrs email
+     * @return returns the UserAuthenicator object from the login attempt
+     */
+    public static UserAuthenticator facebookLogin(String facebookId, String firstName, String lastName, String email){
+        UserAuthenticator.Status status;
+        UserAuthenticator ua = null;
+        User user;
+        try{
+            //check if user exist
+            user = LambencyServer.dbc.searchForUser(facebookId, 2);
+            if(user != null){
+                //user exists
+                //make and set oAuthCode
+                status = UserAuthenticator.Status.SUCCESS;
+                ua = new UserAuthenticator(status);
+                LambencyServer.dbc.setOauthCode(user.getUserId(), ua.getoAuthCode());
+            }
+            else{
+                //user doesnt exist
+                //create user
+                int userId = LambencyServer.dbc.createUser(facebookId, firstName, lastName, email, 2);
+                //make and set oAuthCode
+                status = UserAuthenticator.Status.SUCCESS;
+                ua = new UserAuthenticator(status);
+                LambencyServer.dbc.setOauthCode(userId, ua.getoAuthCode());
+            }
+        }
+        catch (SQLException e){
+            //error occurred
+            System.out.println("Exception from database");
+            status = UserAuthenticator.Status.NON_DETERMINANT_ERROR;
+            ua = new UserAuthenticator(status);
+        }
+        return ua;
+
+    }
+}
