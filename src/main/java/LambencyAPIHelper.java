c490c403bcd87028bb8c3864249a72289416c154
Kevin Vosburgh <kvosbur@purdue.edu>
2018-02-11 14:41:04 -0500

    Db modify user info (#7)
    * Created Database method for modifying user data.
    
    * Created a basic retrofit example. Run LambencyServer and then run LambencyAPIHelper for example.
    
    * Merged with dev

---
+++ src/main/java/LambencyAPIHelper.java
@@ -0,0 +1,45 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class LambencyAPIHelper implements Callback<UserAuthenticator> {
+
+
+    public void start(){
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:20000")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        LambencyAPI lambencyAPI = retrofit.create(LambencyAPI.class);
+
+        Call<UserAuthenticator> call = lambencyAPI.getAllLocations("my test id");
+        call.enqueue(this);
+    }
+
+    @Override
+    public void onResponse(Call<UserAuthenticator> call, Response<UserAuthenticator> response) {
+        if(response.isSuccessful()) {
+            UserAuthenticator ua = response.body();
+            System.out.println(ua.getoAuthCode() + "\n" + ua.getStatus());
+        } else {
+            System.out.println(response.errorBody());
+        }
+    }
+
+    @Override
+    public void onFailure(Call<UserAuthenticator> call, Throwable t) {
+        t.printStackTrace();
+    }
+
+    public static void main(String[] args) {
+        LambencyAPIHelper lh = new LambencyAPIHelper();
+        lh.start();
+    }
+}
