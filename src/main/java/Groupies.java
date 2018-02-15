937f9893e808a110e7eb7186d7a2d9867ecad408
zmswartz <zmswartz@users.noreply.github.com>
2018-02-15 17:55:48 -0500

    Added requestJoinOrg and made changes to how groupies is managed. (#17)
    Now use addGroupie, deleteGroupie, and searchGroupie.

---
+++ src/main/java/Groupies.java
@@ -0,0 +1,40 @@
+public class Groupies {
+    //TODO
+    public int orgID;
+    public int userID;
+    public int type;
+    public boolean confirmed;
+
+    public int getOrgID() {
+        return orgID;
+    }
+
+    public void setOrgID(int orgID) {
+        this.orgID = orgID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+
+}
