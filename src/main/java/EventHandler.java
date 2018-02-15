98aaf4d6c1ac29537a912b2c74778cdc45edcc4d
Kevin Vosburgh <kvosbur@purdue.edu>
2018-02-15 12:18:53 -0500

    Modify events for geo coding (#11)
    * Added a createEvents method for the database
    
    * spacing update
    
    * Added the API call as well as an Event and an Eventhandler
    
    * Changed event_id to int
    
    * Created Event &  EventHandler and added code to the database to handle the creation of events.
    
    * Added GoogleGeoCode api class to handle turning an address to a geographical location. Also fixed issue within create event (misnamed column).
    
    * Modified Insert Event to allow for latitude and longitude.
    
    * Modified Event model class to allow for lattitude and longitude

---
+++ src/main/java/EventHandler.java
@@ -0,0 +1,30 @@
+import com.google.maps.model.LatLng;
+
+import java.sql.SQLException;
+
+public class EventHandler {
+
+    /**
+     *
+     * @param event   Event that is to be added to the database
+     * @return  (int)   Will return event_id on success, -1 on failure
+     */
+
+
+    public static int createEvent(Event event) {
+
+        try {
+            //get latitude and longitude for database
+            LatLng latlng = GoogleGeoCodeUtil.getGeoData(event.getLocation());
+            int event_id = LambencyServer.dbc.createEvent(event.getOrg_id(),event.getName(),event.getStart(),
+                    event.getEnd(),event.getDescription(),event.getLocation(),event.getImage_path(), latlng.lat, latlng.lng);
+            return event_id;
+        } catch (SQLException e) {
+            System.out.println("Error in creating event: "+event.getName());
+            return -1;
+        }
+
+    }
+
+
+}
