4b8660004f6d4bca46e3a2a75035941f1adafeb0
Kevin Vosburgh <kvosbur@purdue.edu>
2018-02-22 22:16:58 -0500

    miscellaneous fixes

--- src/main/java/LambencyServer.java
+++ src/main/java/LambencyServer.java
@@ -33,32 +33,38 @@ public class LambencyServer{
     public void addroutes(){
         // example of responding with a json object made from a java object
         get("/User/login/google", "application/json", (request, response) -> {
+            Printing.println("/User/login/google");
             String token = request.queryParams("idToken");
             GoogleLoginHandler glh = new GoogleLoginHandler();
             return glh.getAuthenticator(token);
         }, new JsonTransformer());
         get("/User/search", "application/json", (request, response) -> {
+            Printing.println("/User/search");
             String oAuthCode = request.queryParams("oAuthToken");
             String id = request.queryParams("id");
             return UserHandler.searchForUser(oAuthCode,id);
         }, new JsonTransformer());
         post("/User/changeInfo", "application/json", (request, response) -> {
+            Printing.println("/User/changeInfo");
             UserModel u = new Gson().fromJson(request.body(), UserModel.class);
             return UserHandler.changeInfo(u);
         }, new JsonTransformer());
         get("/User/followOrg", "application/json", (request, response) -> {
+            Printing.println("/User/followOrg");
             String oAuthCode = request.queryParams("oAuthCode");
-            String orgID = request.queryParams("orgId");
+            String orgID = request.queryParams("orgID");
             Integer ret = UserHandler.followOrg(oAuthCode, Integer.parseInt(orgID));
             return ret;
         }, new JsonTransformer());
         post("/User/requestJoinOrg", "application/json", (request, response) -> {
+            Printing.println("/User/requestJoinOrg");
             String oAuthCode = request.queryParams("oAuthCode");
             String orgID = request.queryParams("orgId");
-            Integer ret = UserHandler.requestJoinOrg(oAuthCode, Integer.parseInt(orgID));
+            int ret = UserHandler.requestJoinOrg(oAuthCode, Integer.parseInt(orgID));
             return ret;
         }, new JsonTransformer());
         get("/User/registerForEvent", "application/json", (request, response) -> {
+            Printing.println("/User/registerForEvent");
             String oAuthCode = request.queryParams("oAuthCode");
             String eventID = request.queryParams("eventId");
             Integer ret = UserHandler.registerEvent(oAuthCode, Integer.parseInt(eventID));
@@ -66,25 +72,32 @@ public class LambencyServer{
         }, new JsonTransformer());
         post("/Organization/create", "application/json",
                 (request, response) -> {
+                    Printing.println("/Organization/create");
                        return OrganizationHandler.createOrganization( new Gson().fromJson(request.body(), OrganizationModel.class));
                 }, new JsonTransformer());
         get("/Organization/search", "application/json", (request, response) -> {
+            Printing.println("/Organization/search");
             ArrayList<OrganizationModel> orgList = OrganizationHandler.searchOrgName(request.queryParams("name"));
             return orgList;
         }, new JsonTransformer());
         get("/Organization/searchByID", "application/json", (request, response) -> {
+            Printing.println("/Organization/searchByID");
             OrganizationModel organization = OrganizationHandler.searchOrgID(Integer.parseInt(request.queryParams("id")));
             return organization;
         }, new JsonTransformer());
         post("/Event/update", "application/json",
-                (request, response) ->
-                        EventHandler.updateEvent( new Gson().fromJson(request.body(), EventModel.class))
+                (request, response) ->{
+                    Printing.println("/Event/update");
+                    return EventHandler.updateEvent( new Gson().fromJson(request.body(), EventModel.class));
+                }
                 , new JsonTransformer());
-        post("/Event/create", "application/json", (request, response) ->
-                        EventHandler.createEvent( new Gson().fromJson(request.body(), EventModel.class))
+        post("/Event/create", "application/json", (request, response) -> {
+                    Printing.println("/Event/create");
+                    return EventHandler.createEvent(new Gson().fromJson(request.body(), EventModel.class));
+                }
                 , new JsonTransformer());
-
         get("/Event/search","application/json", (request,response)->{
+            Printing.println("/Event/search");
             String latStr = request.queryParams("lat");
             String longStr = request.queryParams("long");
             String name = request.queryParams("name");
@@ -92,20 +105,26 @@ public class LambencyServer{
             return EventHandler.getEventsByLocation(Double.parseDouble(latStr), Double.parseDouble(longStr));
         }, new JsonTransformer());
         get("/Event/searchByID", "application/json", (request, response) -> {
+            Printing.println("/Event/searchByID");
             EventModel eventModel = EventHandler.searchEventID(Integer.parseInt(request.queryParams("id")));
             return eventModel;
         }, new JsonTransformer());
         get("/Event/users","application/json",(request,response)->{
+            Printing.println("/Event/users");
             String oauthcode = request.queryParams("oauthcode");
             int event_id = Integer.parseInt(request.queryParams("event_id"));
             return EventHandler.getUsersAttending(oauthcode,event_id);
         }, new JsonTransformer());
 
         get("/User/login/facebook", "application/json", (request, response) -> {
+            Printing.println("/User/login/facebook");
             UserAuthenticator ua = FacebookLogin.facebookLogin(request.queryParams("id"), request.queryParams("first"), request.queryParams("last"), request.queryParams("email"));
             return ua;
         }, new JsonTransformer());
-        get("/User/unfollowOrg","application/json",(request, response) -> UserHandler.unfollowOrg(request.queryParams("oAuthCode"),Integer.parseInt(request.queryParams("org_id"))), new JsonTransformer());
+        get("/User/unfollowOrg","application/json",(request, response) -> {
+            Printing.println("/User/unfollowOrg");
+            return UserHandler.unfollowOrg(request.queryParams("oAuthCode"), Integer.parseInt(request.queryParams("org_id")));
+        }, new JsonTransformer());
 
 
     }
