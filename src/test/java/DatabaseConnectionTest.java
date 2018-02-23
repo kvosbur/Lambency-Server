f50ca804b55fe179344ded13bceab9cb085fa5b7
zmswartz <zmswartz@users.noreply.github.com>
2018-02-23 14:38:23 -0500

    Made testing into a unit test (#67)

--- src/test/java/DatabaseConnectionTest.java
+++ src/test/java/DatabaseConnectionTest.java
@@ -1,4 +1,8 @@
+import org.junit.Test;
+
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -11,22 +15,8 @@ public class DatabaseConnectionTest {
     private static int event_id = 0;
 
     @org.junit.Test
-    public void getUserList() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void searchForUser() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void searchForUser1() throws Exception {
-
-    }
-
-    @org.junit.Test
     public void createUser() throws Exception {
+        setUpTests();
         int userID = dbc.createUser("facebookUser", "First", "Last", "email@gmail.com", 2);
         if (dbc.verifyUserEmail("email@gmail.com") == 1) {
             //System.out.println("verifyUserEmail failed: email@gmail.com should exist");
@@ -48,9 +38,37 @@ public class DatabaseConnectionTest {
             throw new Exception("UserModel information failed: information was incorrect");
         }
     }
+    @org.junit.Test
+    public void createOrganization() throws Exception{
+        createUser();
+        UserModel u = dbc.searchForUser("facebookUser", 2);
+        int orgID = dbc.createOrganization("My OrganizationModel", "This is a description", "Org@gmail.com", u.getUserId(), "West Lafayette",
+                "img", u.getUserId());
+        if(orgID == -1){
+            throw new Exception("createOrganization failed: returned -1");
+        }
+        OrganizationModel org = dbc.searchForOrg(orgID);
+        OrganizationModel org2 = dbc.searchForOrg("My OrganizationModel");
+        if(org == null){
+            throw new Exception("search for org by id failed: returned null");
+        }
+        if(!(org.getName().equals("My OrganizationModel") && org.getDescription().equals("This is a description") && org.getEmail().equals("Org@gmail.com")
+                && org.getContact().getUserId() == u.getUserId() && org.getLocation().equals("West Lafayette") && org.getImage().equals("img"))){
+            throw new Exception("search for org by id failed: incorrect fields");
+        }
+        org = org2;
+        if(org == null){
+            throw new Exception("search for org by name failed: returned null");
+        }
+        if(!(org.getName().equals("My OrganizationModel") && org.getDescription().equals("This is a description") && org.getEmail().equals("Org@gmail.com")
+                && org.getContact().getUserId() == u.getUserId() && org.getLocation().equals("West Lafayette") && org.getImage().equals("img"))){
+            throw new Exception("search for org by name failed: incorrect fields");
+        }
+    }
 
     @org.junit.Test
     public void createEvent() throws Exception {
+        createOrganization();
         UserModel u = dbc.searchForUser("facebookUser", 2);
         OrganizationModel org = dbc.searchForOrg("My OrganizationModel");
         int eventID = dbc.createEvent(org.getOrgID(),"Event 1", new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis() + 500),
@@ -68,6 +86,7 @@ public class DatabaseConnectionTest {
 
     @org.junit.Test
     public void modifyEventInfo() throws Exception {
+        createEvent();
         EventModel e = dbc.searchEvents(event_id);
         if(e == null){
             throw new Exception("search for event failed");
@@ -85,48 +104,170 @@ public class DatabaseConnectionTest {
     }
 
     @org.junit.Test
-    public void setOauthCode() throws Exception {
+    public void followOrg() throws Exception{
+        modifyEventInfo();
+        UserAuthenticator ua = FacebookLogin.facebookLogin("User2", "Jeff", "Turkstra", "jeff@purdue.edu");
+        UserModel u = dbc.searchForUser("User2", 2);
+        OrganizationModel org = dbc.searchForOrg("My OrganizationModel");
+        int ret = UserHandler.followOrg(u.getOauthToken(), org.getOrgID());
+        if(ret == 1){
+            throw new Exception("Unable to find user or organization");
+        }
+        if(ret == 2){
+            throw new Exception("SQL exception");
+        }
+        GroupiesModel g = dbc.searchGroupies(u.getUserId(), org.getOrgID());
+        if(g == null){
+            throw new Exception("Error in groupies: returned null");
+        }
+        if(g.getType() != DatabaseConnection.FOLLOW){
+            throw new Exception("Error in groupies: not set to follow");
+        }
+    }
 
+    @org.junit.Test
+    public void unfollowOrg() throws Exception{
+        followOrg();
+        UserModel u = dbc.searchForUser("User2", 2);
+        OrganizationModel org = dbc.searchForOrg("My OrganizationModel");
+        int ret = UserHandler.unfollowOrg(u.getOauthToken(), org.getOrgID());
+        if(ret == 1){
+            throw new Exception("Unable to find user or organization");
+        }
+        if(ret == 2){
+            throw new Exception("SQL exception");
+        }
+        GroupiesModel g = dbc.searchGroupies(u.getUserId(), org.getOrgID());
+        if(g != null){
+            throw new Exception("Error in groupies: not deleted");
+        }
     }
 
     @org.junit.Test
     public void modifyUserInfo() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void verifyUserEmail() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void createEvent1() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void searchEventsByLocation() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void searchEvents() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void searchEventAttendance() throws Exception {
-
+        unfollowOrg();
+        UserModel u = dbc.searchForUser("facebookUser", 2);
+        u.setEmail("newemail@gmail.com");
+        u.setFirstName("George");
+        u.setLastName("Adams");
+        u = UserHandler.changeInfo(u);
+        if(u == null){
+            throw new Exception("change user info failed: returned null");
+        }
+        if(!(u.getEmail().equals("newemail@gmail.com") && u.getFirstName().equals("George") && u.getLastName().equals("Adams"))){
+            throw new Exception("change user info failed: returned incorrect user object");
+        }
+        u = dbc.searchForUser("facebookUser", 2);
+        if(!(u.getEmail().equals("newemail@gmail.com") && u.getFirstName().equals("George") && u.getLastName().equals("Adams"))){
+            throw new Exception("change user info failed: failed to update database");
+        }
     }
 
     @org.junit.Test
     public void registerForEvent() throws Exception {
-
+        modifyUserInfo();
+        UserModel u = dbc.searchForUser("facebookUser", 2);
+        int ret = UserHandler.registerEvent(u.getOauthToken(), event_id);
+        if(ret == 1){
+            throw new Exception("event registration failed: failed to find user or org");
+        }
+        if(ret == 2){
+            throw new Exception("event registration failed: SQL exception");
+        }
+        if(ret == 3){
+            throw new Exception("event registration failed: user already registered");
+        }
+        EventAttendanceModel ea = dbc.searchEventAttendance(u.getUserId(), event_id);
+        if(ea == null){
+            throw new Exception("event registration failed: failed to update database");
+        }
+        if(!(ea.getUserID() == u.getUserId() || ea.getEventID() == event_id)){
+            throw new Exception("event registration failed: incorrect information in database");
+        }
     }
 
     @org.junit.Test
-    public void searchEventAttendanceUsers() throws Exception {
+    public void searchEventAttendanceUsers() throws Exception{
+        registerForEvent();
+        UserModel u = dbc.searchForUser("facebookUser", 2);
+        List<UserModel> userList = EventHandler.getUsersAttending(u.getOauthToken(), event_id);
+        if(userList == null){
+            throw new Exception("making user list failed: returned null");
+        }
+        if(userList.size() > 1){
+            throw new Exception("making user list failed: returned list of incorrect length");
+        }
+        u = userList.get(0);
+        if(!(u.getEmail().equals("newemail@gmail.com") && u.getFirstName().equals("George") && u.getLastName().equals("Adams"))){
+            throw new Exception("making user list failed: returned incorrect user object");
+        }
+    }
 
+    @org.junit.Test
+    public void requestJoinOrg() throws Exception{
+        searchEventAttendanceUsers();
+        UserModel u = dbc.searchForUser("User2", 2);
+        OrganizationModel org = dbc.searchForOrg("My OrganizationModel");
+        int ret = UserHandler.requestJoinOrg(u.getOauthToken(), org.getOrgID());
+        if(ret == 1){
+            throw new Exception("request to join org failed: unable to find user or org or is already registered");
+        }
+        if(ret == 2){
+            throw new Exception("request to join org failed: SQL error");
+        }
+        GroupiesModel g = dbc.searchGroupies(u.getUserId(), org.getOrgID());
+        if(g == null){
+            throw new Exception("Error in groupies: returned null");
+        }
+        if(g.getType() != DatabaseConnection.MEMBER){
+            throw new Exception("Error in groupies: not set to member");
+        }
+
+        if(g.isConfirmed()){
+            throw new Exception("Error in groupies: set to confirmed when not");
+        }
+    }
+
+    @org.junit.Test
+    public void searchForOrg() throws Exception{
+        requestJoinOrg();
+        OrganizationModel org = dbc.searchForOrg("My OrganizationModel");
+        ArrayList<OrganizationModel> array = dbc.searchForOrgArray("M");
+        if(array == null){
+            throw new Exception("failed to search org by name: returned null");
+        }
+        if(!(array.size() == 1 && array.get(0).getOrgID() == org.getOrgID())){
+            throw new Exception("failed to search org by name: returned incorrect org");
+        }
+        UserModel u = dbc.searchForUser("User2", 2);
+        int orgID = dbc.createOrganization("My Second OrganizationModel", "Second", "Org2@gmail.com", u.getUserId(), "Purdue",
+                "img2", u.getUserId());
+
+        array = dbc.searchForOrgArray("M");
+        if(array == null){
+            throw new Exception("failed to search org by name: returned null");
+        }
+        if(!(array.size() == 2 && array.get(0).getOrgID() == org.getOrgID() && array.get(1).getOrgID() == orgID)){
+            throw new Exception("failed to search org by name: returned incorrect org");
+        }
+        array = dbc.searchForOrgArray("My Sec");
+        if(array == null){
+            throw new Exception("failed to search org by name: returned null");
+        }
+        if(!(array.size() == 1 && array.get(0).getOrgID() == orgID)){
+            throw new Exception("failed to search org by name: returned incorrect org");
+        }
+    }
+
+    public void setUpTests(){
+        try {
+            dbc = new DatabaseConnection();
+            dbc.truncateTables();
+        }
+        catch (Exception e){
+            return;
+        }
+        LambencyServer.dbc = dbc;
     }
 
 }
