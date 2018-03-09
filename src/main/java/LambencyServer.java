5e658fecbc34d8cb1e8746bcba780eef435b6217
Decoder22 <brianjohncox232@gmail.com>
2018-03-09 10:21:10 -0500

    10.5 api part2 (#97)
    * Fixed call for users and created retrofit call to get both members and organizers for an event
    
    * fix address

--- src/main/java/LambencyServer.java
+++ src/main/java/LambencyServer.java
@@ -190,8 +190,8 @@ public class LambencyServer{
             return OrganizationHandler.getRequestedToJoinMembers(oAuthCode,Integer.parseInt(orgID));
         }, new JsonTransformer());
 
-        get("/Organization/members", "application.json", (request, response) -> {
-            Printing.println("Organization/members");
+        get("/Organization/getMembersAndOrganizers", "application.json", (request, response) -> {
+            Printing.println("Organization/getMembersAndOrganizers");
             String oAuthCode = request.queryParams("oAuthCode");
             String orgID = request.queryParams("orgID");
             if(oAuthCode == null || orgID == null){
