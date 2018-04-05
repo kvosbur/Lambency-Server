904d4c86965559184052d4d207aee9656285f00b
Lucas Shanker <lshanke@purdue.edu>
2018-04-04 21:39:48 -0400

    Add firebase and code for sending a cloud message

---
+++ src/main/java/FirebaseHelper.java
@@ -0,0 +1,58 @@
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class FirebaseHelper {
+
+    public static void initializeFirebase(){
+        //Initialize firebase stuff
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("C:\\Users\\lshan\\IdeaProjects\\Lambency-Server\\src\\lambency-f7029-firebase-adminsdk-ptyok-549eba84a5.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://lambency-f7029.firebaseio.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main (String [] args){
+        initializeFirebase();
+
+        // This registration token comes from the client FCM SDKs.
+        String registrationToken = "fSKEStmhAzs:APA91bES98kK8nPMSaXDu25RM4C3PKxALl3yjK95b0L78zu4A4CrTRgm8ETWULqgWVFor2kzJzdb1xeud_cHJbR_sDPrtN8QguQqC_NGT3pvm-rg7wQTUcTNMbSAtjihQnWgRvnHEtY8";
+
+        // See documentation on defining a message payload.
+        Message message = Message.builder()
+                .putData("score", "850")
+                .putData("time", "2:45")
+                .setToken(registrationToken)
+                .build();
+
+        // Send a message to the device corresponding to the provided
+        // registration token.
+        String response = null;
+
+        try {
+            response = FirebaseMessaging.getInstance().sendAsync(message).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        // Response is a message ID string.
+        System.out.println("Successfully sent message: " + response);
+
+
+    }
+}
